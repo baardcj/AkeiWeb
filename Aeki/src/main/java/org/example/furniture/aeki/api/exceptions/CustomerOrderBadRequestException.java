fe229232601bbaa35d8d4b497b23f77f8df5e816
Bard <baardcj@outlook.com>
2024-05-10 10:29:44 +0200

    added new line in delegate

--- Aeki/src/main/java/org/example/furniture/aeki/api/exceptions/CustomerOrderBadRequestException.java
+++ Aeki/src/main/java/org/example/furniture/aeki/api/exceptions/CustomerOrderBadRequestException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class CustomerOrderBedRequestException  extends RuntimeException {
+public class CustomerOrderBadRequestException extends RuntimeException {
 
-    public CustomerOrderBedRequestException(String message) {
+    public CustomerOrderBadRequestException(String message) {
         super(message);
     }
 }
